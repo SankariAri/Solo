571b4d1e47ec272fab7e4f57d5783a38527015cf
Sankari Ayyaluru <sankariayyaluru@gmail.com>
2022-01-26 15:56:36 -0800

    configured style to webpack

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import store from './store';
-
+import styles from './styles.css';
 
 
 render(
