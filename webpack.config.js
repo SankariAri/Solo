cade1f59986da678d75f690ed9a627aacd0c5697
Sankari Ayyaluru <sankariayyaluru@gmail.com>
2022-01-25 16:24:51 -0800

    wip

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+let NODE_ENV;
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path:path.resolve(__dirname, './build'),
+        filename:'bundle.js', 
+    },
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: { presets: ['@babel/preset-env', '@babel/preset-react']},  
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        ],
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'build'),
+          publicPath: '/build',
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'html-webpack-plugin',
+          template: 'index.html',
+        }),
+      ],
+    proxy: {
+        '/api': 'http://localhost:3000'
+    }
+}
