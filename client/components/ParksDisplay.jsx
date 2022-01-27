41916753b6d49baa67f4f144c8fab3dee0bfad31
Sankari Ayyaluru <sankariayyaluru@gmail.com>
2022-01-27 12:44:11 -0800

    added hike and notes input fields

--- client/components/ParksDisplay.jsx
+++ client/components/ParksDisplay.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 const ParksDisplay = props => {
     console.log(props)
+    const spring = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
     return (
-        <div className="displayBox">
-           <h3>{props.name}</h3> 
-
-    </div>
+        <div className="displayBox"> 
+        <h2>{props.name}</h2> 
+        <h4>Add a Hike: <input type = "text"></input> 
+        <button type ="button" onClick = {() => clicked(props)}> Add </button></h4>
+        <textarea cols="30" rows="10"> Add Park Notes Here </textarea> 
+        <div> <button type ="button" onClick = {() => clicked(props)}> Submit </button> </div>
+        </div>
     )
 }
+
+function clicked(props) {
+    const input = document.querySelector('input');
+    props.aM(input.value);
+    input.value = '';
+  }
 
 
 export default ParksDisplay;
